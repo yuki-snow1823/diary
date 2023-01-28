49097a335a112e08ab9199c0664ca501145dd205
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-01-28 16:26:10 +0900

    graphqlの導入

--- backend/Gemfile
+++ backend/Gemfile
@@ -44,5 +44,8 @@ end
 group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+  gem 'graphiql-rails'
+  gem 'sass-rails'
 end
 
+gem 'graphql'
--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,5 +1,4 @@
 require_relative "boot"
-
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
@@ -22,6 +21,8 @@ module Backend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     config.api_only = true
   end
 end
