f197baabf9e593691eda55248fc566f6c3ebea4e
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-03-22 23:00:35 +0900

    不要な差分を元に戻す

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -7,13 +7,11 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :journals, resolver: Resolvers::JournalsResolver
-
-    # field :journals, [Types::JournalType], null: false
-
-    # def journals
-    #   Journal.all
-    # end
-
+    # TODO: remove me
+    field :test_field, String, null: false,
+                               description: 'An example field added by the generator'
+    def test_field
+      'Hello World!'
+    end
   end
 end
