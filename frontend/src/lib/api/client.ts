e5db6cbaca7ca2191c8eacc227a0372017ac65cd
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-05-07 14:41:32 +0900

    クライアント作成

---
+++ frontend/src/lib/api/client.ts
@@ -0,0 +1,12 @@
+import applyCaseMiddleware from "axios-case-converter"
+import axios from "axios"
+
+const options = {
+  ignoreHeaders: true 
+}
+
+const client = applyCaseMiddleware(axios.create({
+  baseURL: "http://localhost:3000/"
+}), options)
+
+export default client
