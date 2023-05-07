96200516177e659d3221c62a0e1afe2cb3136ef8
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-05-07 14:41:58 +0900

    ログイン画面の実装（まだログイン状態の確認は不可）

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,66 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { Sample } from './Sample'
 import { Test } from './Test'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { getCurrentUser } from './lib/api/auth'
+import { createContext, useEffect, useState } from 'react'
+
+export interface User {
+  id: number
+  uid: string
+  provider: string
+  email: string
+  name: string
+  nickname?: string
+  image?: string
+  allowPasswordChange: boolean
+  created_at: Date
+  updated_at: Date
+}
 
 const client = new ApolloClient({
   uri: 'https://tdiaryapi.herokuapp.com/graphql',
   cache: new InMemoryCache()
 })
 
+export const AuthContext = createContext(
+  {} as {
+    loading: boolean
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>
+    isSignedIn: boolean
+    setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+    currentUser: User | undefined
+    setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>
+  }
+)
+
 function App() {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
+  const [currentUser, setCurrentUser] = useState<User | undefined>()
+
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser()
+
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true)
+        setCurrentUser(res?.data.data)
+
+        console.log(res?.data.data)
+      } else {
+        console.log('No current user')
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    handleGetCurrentUser()
+  }, [setCurrentUser])
+
   return (
     <BrowserRouter>
       <ApolloProvider client={client}>
--- frontend/src/Sample.tsx
+++ frontend/src/Sample.tsx
@@ -1,17 +1,75 @@
-import React from 'react'
-import { useQuery, gql } from '@apollo/client'
+import React, { useState, useContext } from 'react'
+import Cookies from 'js-cookie'
+import { signIn } from './lib/api/auth'
+import { AuthContext } from './App'
 
-const testField = gql`
-  query GetTestField {
-    testField
+export interface SignInParams {
+  email: string
+  password: string
+}
+
+const handleSubmit = async (params :SignInParams) => {
+  console.log('aaaaaaa')
+  
+  const { setIsSignedIn, setCurrentUser } = useContext(AuthContext)
+
+  try {
+    const res = await signIn(params)
+    console.log(res)
+
+    if (res.status === 200) {
+      Cookies.set('_access_token', res.headers['access-token'])
+      Cookies.set('_client', res.headers['client'])
+      Cookies.set('_uid', res.headers['uid'])
+
+      setIsSignedIn(true)
+      setCurrentUser(res.data.data)
+
+      console.log('Signed in successfully!')
+    } else {
+      console.log('faild')
+    }
+  } catch (err) {
+    console.log(err)
   }
-`
+}
 
 export const Sample = () => {
-  const { loading, error, data } = useQuery(testField)
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
 
-  if (loading) return <>'ロード中....'</>
-  if (error) return <>`Error ${error.message}`;</>
+  const params: SignInParams = {
+    email: email,
+    password: password
+  }
 
-  return <>{data.testField}</>
+  return (
+    <>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button onClick={() => handleSubmit(params)} type="submit" disabled={!email || !password}>
+          Submit
+        </button>
+      </form>
+    </>
+  )
 }
