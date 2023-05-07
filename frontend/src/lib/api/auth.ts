03a1e8a79c18a29306c15d53e10ecee7d8926b5c
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-05-07 14:41:38 +0900

    ログイン用のライブラリ作成

---
+++ frontend/src/lib/api/auth.ts
@@ -0,0 +1,39 @@
+import client from "./client"
+import Cookies from "js-cookie"
+
+export interface SignUpParams {
+  name: string
+  email: string
+  password: string
+  passwordConfirmation: string
+}
+
+export interface SignInParams {
+  email: string
+  password: string
+}
+
+export const signUp = (params: SignUpParams) => {
+  return client.post("auth", params)
+}
+
+export const signIn = (params: SignInParams)  => {
+  return client.post("auth/sign_in", params)
+}
+
+export const signOut = () => {
+  return client.delete("auth/sign_out", { headers: {
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid")
+  }})  
+}
+
+export const getCurrentUser = () => {
+  if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
+  return client.get("/auth/sessions", { headers: {
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid")
+  }})
+}
