fc19f46515dfa2e25371e80ee0d78234f875ef76
Chikuwa <86566390+isoiso-isobeage@users.noreply.github.com>
2023-06-17 15:49:41 +0900

    記事の更新機能を作成した

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_journal, mutation: Mutations::CreateJournal
+    field :update_journal, mutation: Mutations::UpdateJournal
     field :delete_journal, mutation: Mutations::DeleteJournal
   end
 end
