8b18bc0571d72399b93e06e798bd3feeb237c021
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2023-06-25 17:58:02 +0900

    create_journalにrequieredの記述を追加

--- backend/app/graphql/mutations/create_journal.rb
+++ backend/app/graphql/mutations/create_journal.rb
@@ -2,8 +2,8 @@ module Mutations
   class CreateJournal < Mutations::BaseMutation
     field :journal, Types::JournalType, null: false
 
-    argument :title, String
-    argument :content, String
+    argument :title, String, required: true
+    argument :content, String, required: true
     argument :user_id, Integer, required: true
 
     def resolve(title:, content:, user_id:)
