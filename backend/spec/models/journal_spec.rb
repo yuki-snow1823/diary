73b9febd47923ffbaeced64b76dd3efe397c716a
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2023-06-25 17:25:56 +0900

    JournalモデルのRSpec追加

--- backend/spec/models/journal_spec.rb
+++ backend/spec/models/journal_spec.rb
@@ -6,5 +6,13 @@ describe Journal do
     it '1文字のタイトルは保存されない' do
       expect(build(:journal, title: 't')).to be_invalid
     end
+
+    it 'タイトルがnilの場合は保存されない' do
+      expect(build(:journal, title: nil)).to be_invalid
+    end
+
+    it '内容がnilの場合は保存されない' do
+      expect(build(:journal, content: nil)).to be_invalid
+    end
   end
 end
