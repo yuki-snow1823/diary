aa374abec9a40f3ee3c04b004cc8d70f83b50bbd
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2023-07-09 14:26:53 +0900

    journalモデルのテスト追加（正常系＆空文字）

--- backend/spec/models/journal_spec.rb
+++ backend/spec/models/journal_spec.rb
@@ -3,12 +3,28 @@ require 'spec_helper'
 
 describe Journal do
   describe 'バリデーションのテスト' do
+    it '2文字のタイトルは保存される' do
+      expect(build(:journal, title: 'tt')).to be_valid
+    end
+
     it '1文字のタイトルは保存されない' do
       expect(build(:journal, title: 't')).to be_invalid
     end
 
+    it '空文字のタイトルは保存されない' do
+      expect(build(:journal, title: '')).to be_invalid
+    end
+
     it 'タイトルがnilの場合は保存されない' do
       expect(build(:journal, title: nil)).to be_invalid
+    end
+
+    it '1文字の内容は保存される' do
+      expect(build(:journal, content: 'c')).to be_valid
+    end
+
+    it '空文字の内容は保存される' do
+      expect(build(:journal, content: '')).to be_valid
     end
 
     it '内容がnilの場合は保存されない' do
