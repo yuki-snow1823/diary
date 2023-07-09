fe9e980bd8961bdcbc4d943addc058bd0225e255
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2023-07-09 15:22:50 +0900

    journal_spec正異でcontext分割

--- backend/spec/models/journal_spec.rb
+++ backend/spec/models/journal_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe Journal do
-  describe 'バリデーションのテスト' do
+  context 'バリデーションの正常系テスト' do
     it '2文字のタイトルは保存される' do
       expect(build(:journal, title: 'tt')).to be_valid
     end
 
+    it '1文字の内容は保存される' do
+      expect(build(:journal, content: 'c')).to be_valid
+    end
+
+    it '空文字の内容は保存される' do
+      expect(build(:journal, content: '')).to be_valid
+    end
+  end
+
+  context 'バリデーションの異常系テスト' do
     it '1文字のタイトルは保存されない' do
       expect(build(:journal, title: 't')).to be_invalid
     end
@@ -17,14 +27,6 @@ describe Journal do
 
     it 'タイトルがnilの場合は保存されない' do
       expect(build(:journal, title: nil)).to be_invalid
-    end
-
-    it '1文字の内容は保存される' do
-      expect(build(:journal, content: 'c')).to be_valid
-    end
-
-    it '空文字の内容は保存される' do
-      expect(build(:journal, content: '')).to be_valid
     end
 
     it '内容がnilの場合は保存されない' do
