7597ed475e68d9e0f7297f9ddbffe4a8d333c413
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-07-09 13:02:22 +0900

    ログイン機能のContainerとPresentational追加

---
+++ frontend/src/login/component/LoginContainer.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useContext } from 'react'
+import Cookies from 'js-cookie'
+import { signIn } from '../../lib/api/auth'
+import { AuthContext } from '../../App'
+import LoginPresentational from './LoginPresentational'
+
+export interface SignInParams {
+  email: string
+  password: string
+}
+
+export const LoginContainer = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const { setIsSignedIn, setCurrentUser, currentUser } = useContext(AuthContext)
+
+  const params: SignInParams = {
+    email: email,
+    password: password
+  }
+
+  const handleSubmit = async (params: SignInParams) => {
+    try {
+      const res = await signIn(params)
+      console.log(res)
+
+      if (res.status === 200) {
+        Cookies.set('_access_token', res.headers['access-token'])
+        Cookies.set('_client', res.headers['client'])
+        Cookies.set('_uid', res.headers['uid'])
+
+        setIsSignedIn(true)
+        setCurrentUser(res.data.data)
+
+        console.log('Signed in successfully!')
+      } else {
+        console.log('faild')
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <LoginPresentational
+      currentUser={currentUser}
+      email={email}
+      password={password}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      handleSubmit={() => handleSubmit(params)}
+    />
+  )
+}
