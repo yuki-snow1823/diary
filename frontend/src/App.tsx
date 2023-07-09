9ab3d1a217666a8ee31a79f253b3bc5435ffac5f
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-07-09 13:02:40 +0900

    App全てでログイン機能を使えるようにした

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,86 @@
 import './App.css'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
-import { Sample } from './Sample'
-import { Test } from './Test'
+import { Signin } from './SignIn'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { getCurrentUser } from './lib/api/auth'
+import { createContext, useEffect, useState } from 'react'
+
+export interface User {
+  id: number
+  uid: string
+  provider: string
+  email: string
+  name: string
+  nickname?: string
+  image?: string
+  allowPasswordChange: boolean
+  created_at: Date
+  updated_at: Date
+}
 
 const client = new ApolloClient({
   uri: 'https://tdiaryapi.herokuapp.com/graphql',
   cache: new InMemoryCache()
 })
 
+export const AuthContext = createContext(
+  {} as {
+    loading: boolean
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>
+    isSignedIn: boolean
+    setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+    currentUser: User | undefined
+    setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>
+  }
+)
+
 function App() {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
+  const [currentUser, setCurrentUser] = useState<User | undefined>()
+
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser()
+
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true)
+        setCurrentUser(res?.data.data)
+
+        console.log(res?.data.data)
+      } else {
+        console.log('No current user')
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    handleGetCurrentUser()
+  }, [setCurrentUser])
+
   return (
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <Routes>
-          <Route path="/sample" element={<Sample />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </ApolloProvider>
-    </BrowserRouter>
+    <AuthContext.Provider
+      value={{
+        loading,
+        setLoading,
+        isSignedIn,
+        setIsSignedIn,
+        currentUser,
+        setCurrentUser
+      }}
+    >
+      <BrowserRouter>
+        <ApolloProvider client={client}>
+          <Routes>
+            <Route path="/sign_in" element={<Signin />} />
+          </Routes>
+        </ApolloProvider>
+      </BrowserRouter>
+    </AuthContext.Provider>
   )
 }
 
