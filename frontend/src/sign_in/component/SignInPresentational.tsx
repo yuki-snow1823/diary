3566234937da352ba2129010575c84fe138bbf4d
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-07-22 08:22:09 +0900

    signinをトップに配置し、MUIでフォームを作成した

--- frontend/src/sign_in/component/SignInPresentational.tsx
+++ frontend/src/sign_in/component/SignInPresentational.tsx
@@ -1,54 +1,64 @@
-import React from 'react'
-import { User } from '../../App'
+import React from 'react';
+import { User } from '../../App';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 type Props = {
-  currentUser: User | undefined
-  email: string
-  password: string
-  handleEmailChange: React.ChangeEventHandler<HTMLInputElement>
-  handlePasswordChange: React.ChangeEventHandler<HTMLInputElement>
-  handleSubmit: () => Promise<void>
-}
+  currentUser: User | undefined;
+  email: string;
+  password: string;
+  handleEmailChange: React.ChangeEventHandler<HTMLInputElement>;
+  handlePasswordChange: React.ChangeEventHandler<HTMLInputElement>;
+  handleSubmit: () => Promise<void>;
+};
 
 const SignInPresentational: React.FC<Props> = (props) => {
   return (
-    <>
-      <form>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
         {props.currentUser ? (
-          <h2>{props.currentUser.email}でログインしてるよ</h2>
-        ) : (
-          ''
-        )}
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            value={props.email}
-            onChange={props.handleEmailChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            value={props.password}
-            onChange={props.handlePasswordChange}
-            required
-          />
-        </div>
-        <button
-          onClick={props.handleSubmit}
-          type="button"
-          disabled={!props.email || !props.password}
-        >
-          Submit
-        </button>
-      </form>
-    </>
-  )
-}
+          <Typography variant="h4" component="h2" gutterBottom>
+            {props.currentUser.email}<br/>でログインしてるよ
+          </Typography>
+        ) : null}
 
-export default SignInPresentational
+        <TextField
+          fullWidth
+          margin="normal"
+          id="email"
+          type="email"
+          label="Email"
+          value={props.email}
+          onChange={props.handleEmailChange}
+          required
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="password"
+          type="password"
+          label="Password"
+          value={props.password}
+          onChange={props.handlePasswordChange}
+          required
+        />
+
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          disabled={!props.email || !props.password}
+          onClick={props.handleSubmit}
+        >
+          Sign In
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default SignInPresentational;
