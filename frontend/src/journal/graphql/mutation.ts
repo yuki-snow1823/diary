6f92ac94e9609ce694fa5b122bc6051b2aa1df73
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-07-23 12:25:23 +0900

    userIdを第一引数にした上でMUTATIONの利用

---
+++ frontend/src/journal/graphql/mutation.ts
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client'
+
+export const CREATE_JOURNAL = gql`
+  mutation createJournal($userId: Int!, $title: String!, $content: String!) {
+    createJournal(input: {userId: $userId, title: $title, content: $content}) {
+      journal {
+        id
+        title
+        content
+      }
+    }
+  }
+`
