306916b430c3a339b17934f456b5b23758568143
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-07-23 15:18:33 +0900

    ディレクトリ構成の変更と、containerへの切り出し

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { SignInContainer } from './sign_in/component/SignInContainer'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { getCurrentUser } from './lib/api/auth'
 import { createContext, useEffect, useState } from 'react'
-import { NewJournalContainer } from './journal/component/NewJournalContainer'
+import { NewJournalContainer } from './journal/new_journal/component/JournalNewContainer'
 
 export interface User {
   id: number
--- frontend/src/journal/new_journal/component/JournalNewContainer.tsx
+++ frontend/src/journal/new_journal/component/JournalNewContainer.tsx
@@ -1,32 +1,43 @@
 import { useMutation } from '@apollo/client'
 import { CREATE_JOURNAL } from '../graphql/mutation'
-import { useContext } from 'react'
-import { AuthContext } from '../../App'
-import NewJournalPresentational from './NewJournalPresentational'
+import { useContext, useState } from 'react'
+import { AuthContext } from '../../../App'
+import NewJournalPresentational from './JournalNewPresentational'
+
 
 export const NewJournalContainer = () => {
   const { currentUser } = useContext(AuthContext)
   const [createJournal] = useMutation(CREATE_JOURNAL)
 
-  const handleCreateJournal = async (title: string, content: string) => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const handleCreateJournal = async () => {
     try {
       const res = await createJournal({
         variables: {
-          title,
-          content,
+          title: title,
+          content: content,
           userId: currentUser?.id
         }
       })
+      setTitle('')
+      setContent('')
       console.log(res)
     } catch (err) {
       console.log(err)
     }
   }
+  
 
   return (
     <NewJournalPresentational
       currentUser={currentUser}
       handleCreateJournal={handleCreateJournal}
+      title={title}
+      content={content}
+      setTitle={setTitle}
+      setContent={setContent}
     />
   )
 }
