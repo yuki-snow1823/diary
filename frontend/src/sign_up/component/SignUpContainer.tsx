4ffef2dc4a9c665266e2f0c1938feed04b14629f
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-07-29 12:26:27 +0900

    SignUpを参考に新規登録画面の実装

---
+++ frontend/src/sign_up/component/SignUpContainer.tsx
@@ -0,0 +1,32 @@
+import { SignUpHooks } from '../hooks/SignUpHooks'
+import SignUpPresentational from './SignUpPresentational'
+
+export const SignUpContainer = () => {
+  const {
+    handleNameChange,
+    handleEmailChange,
+    handlePasswordChange,
+    handlePasswordConfirmationChange,
+    handleSubmit,
+    currentUser,
+    name,
+    email,
+    password,
+    passwordConfirmation,
+  } = SignUpHooks()
+
+  return (
+    <SignUpPresentational
+      currentUser={currentUser}
+      name={name}
+      email={email}
+      password={password}
+      passwordConfirmation={passwordConfirmation}
+      handleNameChange={handleNameChange}
+      handleEmailChange={handleEmailChange}
+      handlePasswordChange={handlePasswordChange}
+      handlePasswordConfirmationChange={handlePasswordConfirmationChange}
+      handleSubmit={handleSubmit}
+    />
+  )
+}
