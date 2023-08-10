7e0fe5c6ccca54c670ac1455807c6edaab2be3e2
yuki-snow1823 <yuki-snow1823@users.noreply.github.com>
2023-08-10 12:16:54 +0000

    Apply CI Prettier Change

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -1,19 +1,20 @@
-import applyCaseMiddleware from 'axios-case-converter';
-import axios from 'axios';
+import applyCaseMiddleware from 'axios-case-converter'
+import axios from 'axios'
 
 const options = {
   ignoreHeaders: true
-};
+}
 
-const baseURL = process.env.NODE_ENV === 'production' 
-  ? process.env.REACT_APP_API_ENDPOINT
-  : 'http://localhost:3000/';
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_ENDPOINT
+    : 'http://localhost:3000/'
 
 const client = applyCaseMiddleware(
   axios.create({
     baseURL: baseURL
   }),
   options
-);
+)
 
-export default client;
+export default client
