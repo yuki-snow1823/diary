4ee7ec8ee6d5951b61ff4048d7cdff16c84086b5
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-08-14 22:16:03 +0900

    型情報では代入などしないので消した+再代入しないマジックコメントの削除

--- backend/app/graphql/types/user_type.rb
+++ backend/app/graphql/types/user_type.rb
@@ -1,10 +1,11 @@
 module Types
-  class JournalType < Types::BaseObject
+  class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String
-    field :content, String
+    field :email, String
+    field :name, String
+    field :nickname, String
+    field :image, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :user_id, Integer, null: false
   end
 end
