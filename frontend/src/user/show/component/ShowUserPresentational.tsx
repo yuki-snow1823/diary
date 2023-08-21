8593af09cc3a3f477fac21e44ec8364ce04d8c8c
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-08-21 13:30:43 +0900

    簡易なページの表示

---
+++ frontend/src/user/show/component/ShowUserPresentational.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { User } from 'App'
+
+type Props = {
+  user: any
+}
+
+const ShowUserPresentational: React.FC<Props> = ({ user }) => {
+  console.log(user, "あああ")
+  if (!user) {
+    return (
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Typography variant="h4" align="center">
+            ユーザーが見つかりません
+          </Typography>
+        </Box>
+      </Container>
+    )
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" align="center" gutterBottom>
+          ユーザー詳細
+        </Typography>
+
+        <Typography variant="h6">名前:</Typography>
+        <Typography>{user.user.name}</Typography>
+
+        {user.nickname && (
+          <>
+            <Typography variant="h6">ニックネーム:</Typography>
+            <Typography>{user.user.nickname}</Typography>
+          </>
+        )}
+
+        <Typography variant="h6">Eメール:</Typography>
+        <Typography>{user.user.email}</Typography>
+      </Box>
+    </Container>
+  )
+}
+
+export default ShowUserPresentational
