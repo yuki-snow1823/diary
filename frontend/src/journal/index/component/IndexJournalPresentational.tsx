9a137c35a633ac6fee96edc6bb4cba9505129426
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-12-16 11:25:20 +0900

    最低限のデザインつけた

--- frontend/src/journal/index/component/IndexJournalPresentational.tsx
+++ frontend/src/journal/index/component/IndexJournalPresentational.tsx
@@ -1,49 +1,47 @@
-import { Container, Box, Typography } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import { Container, Box, Typography, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material';
 
 interface journal {
-  id: number
-  title: string
-  content: string
+  id: number;
+  title: string;
+  content: string;
 }
 
 type Props = {
-  journals: journal[]
+  journals: journal[];
 }
 
-const IndexJournalPresentational: React.FC<Props> = (props) => {
-  const journals = props.journals
+const IndexJournalPresentational: React.FC<Props> = ({ journals }) => {
   return (
-    <>
-      <Container maxWidth="sm">
-        <Box sx={{ mt: 8 }}>
-          <Typography variant="h4" component="div" gutterBottom>
-            投稿記事一覧
-          </Typography>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>title</th>
-                <th>content</th>
-              </tr>
-            </thead>
-            <tbody>
-              {journals.map((journal) => {
-                return (
-                  <tr>
-                    <td>{journal.id}</td>
-                    <td>{journal.title}</td>
-                    <td>{journal.content}</td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-        </Box>
-      </Container>
-    </>
-  )
+    <Container maxWidth="sm">
+      <Box sx={{ mt: 8, mb: 4 }}>
+        <Typography variant="h4" gutterBottom>
+          投稿記事一覧
+        </Typography>
+
+        <Paper elevation={2}>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Title</TableCell>
+                <TableCell>Content</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {journals.map((journal) => (
+                <TableRow key={journal.id}>
+                  <TableCell>{journal.id}</TableCell>
+                  <TableCell>{journal.title}</TableCell>
+                  <TableCell>{journal.content}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </Box>
+    </Container>
+  );
 }
 
-export default IndexJournalPresentational
+export default IndexJournalPresentational;
