ff28d6568aec879a88c34791f022d2e673a61534
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-12-30 10:28:58 +0900

    ヘッダーの追加と読み込み

---
+++ frontend/src/common/header.tsx
@@ -0,0 +1,26 @@
+import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+
+import { useContext } from 'react'
+import { AuthContext } from 'App'
+
+const Header = () => {
+  const { isSignedIn } = useContext(AuthContext)
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          My App
+        </Typography>
+        {!isSignedIn && <Button color="inherit" component={RouterLink} to="/">ログイン</Button>}
+        {!isSignedIn && <Button color="inherit" component={RouterLink} to="/sign_up">新規登録</Button>}
+        <Button color="inherit" component={RouterLink} to="/users">ユーザー</Button>
+        {isSignedIn && <Button color="inherit" component={RouterLink} to="/journals/new">日記を書く</Button>}
+        <Button color="inherit" component={RouterLink} to="/journals">日記一覧</Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
