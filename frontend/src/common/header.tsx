51f02a0ca7103f561cddb6f2256ecc7de8109595
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-12-30 10:34:26 +0900

    リンク調整

--- frontend/src/common/header.tsx
+++ frontend/src/common/header.tsx
@@ -11,13 +11,34 @@ const Header = () => {
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My App
+          <RouterLink
+            to="/"
+            style={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            Diary
+          </RouterLink>
         </Typography>
-        {!isSignedIn && <Button color="inherit" component={RouterLink} to="/">ログイン</Button>}
-        {!isSignedIn && <Button color="inherit" component={RouterLink} to="/sign_up">新規登録</Button>}
-        <Button color="inherit" component={RouterLink} to="/users">ユーザー</Button>
-        {isSignedIn && <Button color="inherit" component={RouterLink} to="/journals/new">日記を書く</Button>}
-        <Button color="inherit" component={RouterLink} to="/journals">日記一覧</Button>
+        {!isSignedIn && (
+          <Button color="inherit" component={RouterLink} to="/">
+            ログイン
+          </Button>
+        )}
+        {!isSignedIn && (
+          <Button color="inherit" component={RouterLink} to="/sign_up">
+            新規登録
+          </Button>
+        )}
+        <Button color="inherit" component={RouterLink} to="/users">
+          ユーザー
+        </Button>
+        {isSignedIn && (
+          <Button color="inherit" component={RouterLink} to="/journals/new">
+            日記を書く
+          </Button>
+        )}
+        <Button color="inherit" component={RouterLink} to="/journals">
+          みんなの日記
+        </Button>
       </Toolbar>
     </AppBar>
   )
