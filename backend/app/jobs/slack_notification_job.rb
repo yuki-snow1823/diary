7b6ebf6ce4c51aeb2fe3c679583b76ac88b83be9
besumero628 <besumero628@gmail.com>
2024-01-27 16:45:49 +0900

    rubocop実行

--- backend/app/jobs/slack_notification_job.rb
+++ backend/app/jobs/slack_notification_job.rb
@@ -2,11 +2,11 @@ class SlackNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(name)
-    conn = Faraday.new(url: "https://hooks.slack.com")
-    response = conn.post do |req|
+    conn = Faraday.new(url: 'https://hooks.slack.com')
+    conn.post do |req|
       req.url '/services/{固有キー}/{固有キー}'
       req.headers['Content-Type'] = 'application/json'
-      req.body = { text: "#{name} さんがDiaryに記事を投稿したよ！"}.to_json
+      req.body = { text: "#{name} さんがDiaryに記事を投稿したよ！" }.to_json
     end
   end
 end
