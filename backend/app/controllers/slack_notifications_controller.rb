5e3a7aab79d8f28a366f18a4e91a79213bccc333
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-04-09 09:02:05 +0900

    コントローラーを削除して、直接呼び出すように実行

--- backend/app/controllers/slack_notifications_controller.rb
+++
@@ -1,7 +0,0 @@
-class SlackNotificationsController < ApplicationController
-  class << self
-    def call(name)
-      SlackNotificationJob.perform_later(name)
-    end
-  end
-end
--- backend/app/graphql/mutations/create_journal.rb
+++ backend/app/graphql/mutations/create_journal.rb
@@ -10,7 +10,7 @@ module Mutations
       journal = Journal.create!({ title:, content:, user_id: })
 
       target_user_name = journal.user.name
-      SlackNotificationsController.call(target_user_name)
+      SlackNotificationJob.perform_later(target_user_name)
 
       { journal: }
     end
